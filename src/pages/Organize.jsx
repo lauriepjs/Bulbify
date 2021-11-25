6158b390d27b9a0603a95295ecf85de32a58e282
Laurie Pujos <pujos.laurie@gmail.com>
2021-11-25 23:03:07 +0100

    css

--- src/pages/Organize.jsx
+++ src/pages/Organize.jsx
@@ -5,7 +5,19 @@ const Organize = () => {
   return (
     <div className="organise">
       <Navigation />
-      <h1>Bonjour</h1>
+      <div className="">
+        <div className="Titre">
+          <h1>Ce soir c'est chez : Léo</h1>
+        </div>
+        <div className="nomsoiréeBlame">
+          <h2>Nom soirée : </h2>
+          <input placeholder=" Soirée 30 ans de Léo..." />
+        </div>
+        <div className="nombreBlame">
+          <h2>Nombre blame / pers : </h2>
+          <input placeholder=" .......... " />
+        </div>
+      </div>
     </div>
   );
 };
