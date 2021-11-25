09d951286c59b6ef857c92a42d5a112043fd1af0
Laurie Pujos <pujos.laurie@gmail.com>
2021-11-25 16:38:16 +0100

    ajout navigation sur boutton principaux

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,26 @@
 import React from "react";
 import Navigation from "../components/Navigation";
+import { Button } from "@mui/material";
+import { NavLink } from "react-router-dom";
+import Organize from "./organize";
 
 const Home = () => {
-    return (
-        <div>
-           <Navigation /> 
-        </div>
-    );
+  return (
+    <div>
+      <Navigation />
+      <img src="src/assets/logobulbify2.png" alt="logo bulbify" />
+      <div className="organise">
+        <NavLink exact to="/organise">
+          <Button variant="contained">J'organise</Button>
+        </NavLink>
+      </div>
+      <div className="join">
+        <NavLink exact to="/joue">
+          <Button variant="contained">Je rejoins</Button>
+        </NavLink>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
