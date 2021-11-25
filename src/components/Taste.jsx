58e95dba1651f1f7e10dd1873fe3d1294a9c697b
leoplanelles <planellesleo@gmail.com>
2021-11-25 20:12:02 +0100

    component taste

--- src/components/Taste.jsx
+++ src/components/Taste.jsx
@@ -1,24 +1,25 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-// import { useParams } from "react-router";
+import { useParams } from "react-router";
+import Join from './Join';
 
 function Taste() {
-    // let { id } = useParams();
+    let { id } = useParams();
 
   const [data, setData] = useState(null);
 
   useEffect(() => {
     axios
-      .get(`https://tastedive.com/api/similar?q=Adele&info=1`)
+      .get(`https://tastedive.com/api/similar?q=${id}`)
       .then((res) => setData(res.data.results));
   });
 
   return (
-    <div>
+    <>
       {data.map((e) => 
         <p>{e.name}</p>)}
-    </div>
-  )
+    </>
+  );
 
 }
 
