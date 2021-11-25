50426c1f982a1db44537c4f66b6f388fe74c5c9d
kachiri <k.mombrial@gmail.com>
2021-11-25 15:03:57 +0100

    Router okay, normalement :)

---
+++ src/pages/Tuto.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navigation from "../components/Navigation";
+
+const Tuto = () => {
+    return (
+        <div>
+            <Navigation />
+        </div>
+    );
+};
+
+export default Tuto;
