9a932d99ab08fdc00d3dd63c3b11ab56045a0620
cyril cabrolier <cyr.cab@hotmail.fr>
2021-11-26 11:17:09 +0100

    last commit

--- src/components/Taste.jsx
+++ src/components/Taste.jsx
@@ -1,14 +1,16 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
-function Taste() {
+function Taste({ id }) {
   const [data, setData] = useState([]);
 
   useEffect(() => {
     axios
       .get(`https://magical-it-works.jsrover.wilders.dev/https://tastedive.com/api/similar?q=Adele&info=1`)
-      .then((res) => console.log(res.data.Similar.Results));
+      .then((res) => setData(res.data.Similar.Results));
   }, []);
+
+  console.log(data);
   return (
     <>
       <iframe width="560" height="315" src="https://www.youtube.com/embed/hLQl3WQQoQ0" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"  allowFullScreen></iframe>
