bbe73aceeaa3353ea8c3e14706bcfb1d3b3b5e0d
Laurie Pujos <pujos.laurie@gmail.com>
2021-11-26 12:46:04 +0100

    ajout donne moi un artiste

--- src/pages/Join.jsx
+++ src/pages/Join.jsx
@@ -1,8 +1,15 @@
-import React from "react";
 import Navigation from "../components/Navigation";
 import { NavLink } from "react-router-dom";
+import { useState } from 'react';
+import { TextField } from '@mui/material';
 
 const Join = () => {
+  const [searchArtist, setSearchArtist] = useState([]);
+
+  const handleSearchArtist = (e) => {
+    setSearchArtist(e.target.value);
+  }
+
   return (
     <div className="jerejoins">
       <Navigation />
@@ -10,17 +17,14 @@ const Join = () => {
         <h2>Ce soir chez Léo</h2>
         <h3>Propose ton son !</h3>
       </div>
-      <div className="input">
-        <input placeholder="Tape ton son .." />
-      </div>
-      <div className="pochetteAlbum">
-        <p>photo pochette album</p>
-        <p>photo pochette album</p>
-        <p>photo pochette album</p>
+      <div className="inputs">
+        <form action="" method="get" onSubmit={handleSearchArtist}>
+        <input  className="donneartiste" placeholder=" Donne moi un artiste !" />
+        </form>
       </div>
       <div className="buttonvalidemusique">
         <NavLink exact to="/joue">
-          <button className="buttonvalide"> Je valide ma liste</button>
+          <button className="buttonvalide" onSubmit={handleSearchArtist}> Je valide ma liste</button>
         </NavLink>
       </div>
     </div>
